75c2a6bd9811a988e7a1a5c77874fdc1d95ff610
OrangeMintz <Nickzgacus@gmail.com>
2025-08-22 04:30:02 +0800

    navbar UI update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import { Logo } from "@/components/logo";
 import { Menu, X } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import React from "react";
 import ThemeToggle from "./ThemeToggle";
 import { buttonVariants } from "@/components/ui/button";
@@ -23,7 +22,7 @@ export const Header = () => {
         className="bg-background/50 fixed z-20 w-full border-b backdrop-blur-3xl"
       >
         <div className="mx-auto max-w-7xl px-6 transition-all duration-300">
-          <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-3">
+          <div className="relative flex flex-wrap items-center justify-between gap-6 py-[10px] lg:gap-0 lg:py-[10px]">
             <div className="flex w-full items-center justify-between gap-12 lg:w-auto">
               <Link
                 href="/"
@@ -48,7 +47,7 @@ export const Header = () => {
                     <li key={index}>
                       <Link
                         href={item.href}
-                        className="text-muted-foreground hover:text-accent-foreground block duration-150"
+                        className="text-muted-foreground hover:text-accent-foreground block duration-150 font-extrabold"
                       >
                         <span>{item.name}</span>
                       </Link>
@@ -60,12 +59,12 @@ export const Header = () => {
 
             <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
               <div className="lg:hidden">
-                <ul className="space-y-6 text-base">
+                <ul className="space-y-6 text-base text-center">
                   {menuItems.map((item, index) => (
                     <li key={index}>
                       <Link
                         href={item.href}
-                        className="text-muted-foreground hover:text-accent-foreground block duration-150"
+                        className="text-muted-foreground hover:text-accent-foreground block duration-150 font-extrabold"
                       >
                         <span>{item.name}</span>
                       </Link>
@@ -73,7 +72,7 @@ export const Header = () => {
                   ))}
                 </ul>
               </div>
-              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit ">
+              <div className="flex w-full flex-col space-y-3 md:flex-row md:gap-3 md:space-y-0 md:w-fit md:mx-auto">
                 <div className="flex justify-center">
                   <ThemeToggle />
                 </div>
